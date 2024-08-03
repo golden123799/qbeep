ccf2a797f3e63df39ca4fbfdb993eb09d11e9c9f
Golden Fam Fook Cheng <fookcheng@9loop.asia>
2024-08-03 18:00:06 +0800

    added empty value

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -25,7 +25,7 @@
 						<div>
 							<label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
 							<select id="company" name="company_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-								<option selected>Choose a company</option>
+								<option selected value="">Choose a company</option>
 								@foreach($companies as $company)
 								<option value="{{$company->id}}" {{ $company->id == old('company_id', $employee->company_id) ? 'selected' : '' }}>{{$company->name}}</option>
 								@endforeach
